86da2999d697fba0a396890b9e7ffb26a68835e6
Pierre Rouanet <pierre.rouanet@gmail.com>
2017-02-28 16:32:56 +0100

    Add m7 and m8

--- doggy_scratch.js
+++ doggy_scratch.js
@@ -183,7 +183,7 @@
             lessMore: ['<', '>'],
 
             acc: ['x', 'y', 'z'],
-            motors: ['m1', 'm2', 'm3', 'm4', 'm5', 'm6'],
+            motors: ['m1', 'm2', 'm3', 'm4', 'm5', 'm6', 'm7', 'm8'],
             distanceSensors: ['front', 'left', 'right'],
         },
         url: '',
